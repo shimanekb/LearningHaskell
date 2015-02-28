a3ab12834ecd2e061687a099e0c38a3d8c71fd64
shimanekb <brandon.shimanek@hotmail.com>
2015-02-28 08:51:48 -0600

    Finished exercise 4 for lists

---
+++ ProgrammingHaskell/src/ch04/ListExercise4.hs
@@ -0,0 +1,22 @@
+import Data.List
+import System.Environment (getArgs)
+
+interactWith function inputFile outputFile = do
+  input <- readFile inputFile
+  writeFile outputFile (function input)
+
+main = mainWith myFunction
+  where mainWith function = do
+          args <- getArgs
+          case args of
+            [input,output] -> interactWith function input output
+            _ -> putStrLn "error: exactly two arguments needed"
+
+        -- replace "id" with the name of our function below
+        myFunction = myTranspose
+             
+
+myTranspose :: String -> String
+myTranspose xs = let parsedString = lines xs
+                     transposedString  = transpose parsedString
+                 in intercalate "\n" transposedString
